515d7f4b44186c05a983fdd08e14eebf3c65ed50
Fernando Arévalo <fernando.arevalo9311@gmail.com>
2018-05-26 07:02:59 -0500

    Implemented Service to read JSON file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductService } from "./product.service";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    ProductService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
